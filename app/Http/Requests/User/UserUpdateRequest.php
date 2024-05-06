53f794d222fd0f705c75a18ad78d2e0f2a72c569
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-06 22:33:48 +0500

    feat: FormRequests
    Update UserUpdateRequest

--- app/Http/Requests/User/UserUpdateRequest.php
+++ app/Http/Requests/User/UserUpdateRequest.php
@@ -12,8 +12,8 @@ class UserUpdateRequest extends FormRequest
         return [
 			"username" => "string",
 			"email" => "email|unique:users,email",
-            "old_password" => ["required_with:password", Password::defaults()],
-			"password" => ["required_with:old_password", Password::defaults()],
+            "password" => ["required_with:old_password", Password::defaults()],
+            "old_password" => ["required_with:password", "same:password"],
         ];
     }
 }
