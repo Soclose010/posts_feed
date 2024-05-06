61c06464990a31b14ff060049c3cca6ec61e5afe
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-06 21:14:54 +0500

    feat: Routing
    Delete api routes. Add web routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,43 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get("/", [PostController::class, "index"]);
+Route::resource("users", UserController::class)->except([
+    "index"
+]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(UserController::class)->prefix("users")->group(function (){
+    Route::get("/{user}", "show");
+
+    Route::middleware("guest")->group(function (){
+        Route::get("/create", "create");
+        Route::post("/", "store");
+    });
+    Route::middleware("auth")->group(function ()
+    {
+        Route::get("/{user}/edit", "edit");
+        Route::put("/{user}", "update");
+        Route::delete("/{post}", "destroy");
+    });
+});
+
+Route::controller(PostController::class)->prefix("posts")->group(function (){
+    Route::get("/{post}", "show");
+    Route::middleware("auth")->group(function ()
+    {
+        Route::get("/create", "create");
+        Route::post("/", "store");
+        Route::get("/{post}/edit", "edit");
+        Route::put("/{post}", "update");
+        Route::delete("/{post}", "destroy");
+    });
+});
+
+Route::controller(AuthController::class)->prefix("auth")->group(function (){
+    Route::post("/login", "login")->middleware("guest");
+    Route::get("/logout", "logout")->middleware("auth");
 });
