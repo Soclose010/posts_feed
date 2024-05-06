b87e205f7f1bda805c729c564ee6601a80b69761
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-06 22:31:28 +0500

    feat: Model
    Add soft delete trait. Add fillable to user and post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        "title",
+        "body",
+        "user_id"
+    ];
 
     public function user()
     {
