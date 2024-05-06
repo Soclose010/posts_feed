5e5f13f5461f055f4d1c00f2d0add7c98b2a4b35
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-06 19:01:48 +0500

    feat: DTO
    Update UserDto. Add methods to create DTO

--- app/DataTransferObjects/UserDto.php
+++ app/DataTransferObjects/UserDto.php
@@ -3,20 +3,63 @@
 namespace App\DataTransferObjects;
 
 use App\Enums\UserRole;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Date;
+use App\Http\Requests\Auth\AuthRequest;
+use App\Http\Requests\User\UserCreateRequest;
+use App\Http\Requests\User\UserUpdateRequest;
+use App\Models\User;
+use Carbon\CarbonInterface;
 
 class UserDto
 {
-    public function __construct(
-        public readonly string   $id,
-        public readonly string   $username,
-        public readonly string   $email,
-        public readonly string   $password,
-        public readonly UserRole $role,
-        public readonly Carbon   $created_at,
-        public readonly Carbon   $updated_at,
-    )
+    public readonly string $id;
+    public readonly string $username;
+    public readonly string $email;
+    public readonly string $password;
+    public readonly UserRole $role;
+    public readonly CarbonInterface $created_at;
+    public readonly CarbonInterface $updated_at;
+
+    private function __construct()
     {
     }
+
+    public static function fromAuthRequest(AuthRequest $request): self
+    {
+        $dto = new self();
+        $dto->email = $request->validated("email");
+        $dto->password = $request->validated("password");
+        return $dto;
+    }
+
+    public static function fromCreateRequest(UserCreateRequest $request): self
+    {
+        $dto = new self();
+        $dto->username = $request->validated("username");
+        $dto->email = $request->validated("email");
+        $dto->password = $request->validated("password");
+        $dto->role = UserRole::User;
+        return $dto;
+    }
+
+    public static function fromModel(User $user): self
+    {
+        $dto = new self();
+        $dto->id = $user->id;
+        $dto->username = $user->username;
+        $dto->email = $user->email;
+        $dto->role = $user->role;
+        $dto->created_at = $user->created_at;
+        return $dto;
+    }
+
+    public static function fromUpdateRequest(UserUpdateRequest $request): self
+    {
+        $dto = new self();
+        $dto->id = $request->validated("id");
+        $dto->username = $request->validated("username");
+        $dto->email = $request->validated("email");
+        $dto->password = $request->validated("password");
+        return $dto;
+    }
+
 }
