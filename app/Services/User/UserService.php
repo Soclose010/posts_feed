d06ae4c0bd2b732b98fff848b1944d36ebfdc489
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-06 22:34:30 +0500

    feat: UserSrvice
    Add implementation of this service

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -3,26 +3,42 @@
 namespace App\Services\User;
 
 use App\DataTransferObjects\UserDto;
+use App\Models\User;
 
 class UserService
 {
     public function create(UserDto $dto): UserDto
     {
-        return $dto;
+        $attributes = array_filter($dto->toArray(), function ($value) {
+            return $value !== null;
+        });
+        $user = User::create($attributes);
+        return UserDto::fromModel($user);
     }
 
-    public function update(UserDto $dto): void
+    public function update(UserDto $dto): UserDto
     {
-
+        $user = $this->getUser($dto->id);
+        $attributes = array_filter($dto->toArray(), function ($value) {
+            return $value !== null;
+        });
+        $user = tap($user->fill($attributes))->save();
+        return UserDto::fromModel($user);
     }
 
     public function delete(string $id): void
     {
-
+        User::where('id', $id)->delete();
     }
 
     public function get(string $id): UserDto
     {
-        return UserDto::fromArray([]);
+        $user = User::where("id", $id)->first();
+        return UserDto::fromModel($user);
+    }
+
+    public function getUser(string $id): User
+    {
+        return User::find($id);
     }
 }
