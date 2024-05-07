9873651d4940cc042ca0b582f7d51dbd09786933
Stepan <pirogov1923@mail.ru>
2024-05-07 20:53:52 +0500

    feat: Gates
    Rewrite two gates in one

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -15,12 +14,8 @@ class AuthServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Gate::define('edit-user', function (User $authUser, User $editedUser) {
-            return $authUser->isAdmin() || $authUser->id === $editedUser->id;
-        });
-
-        Gate::define('edit-post', function (User $authUser, Post $editedPost) {
-            return $authUser->isAdmin() || $authUser->id === $editedPost->user_id;
+        Gate::define('edit', function (User $user, string $id) {
+            return $user->isAdmin() || $user->id == $id;
         });
     }
 }
