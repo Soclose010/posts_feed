ecef6c86403d8750670a351ea5b9ed965186ada2
Stepan <ivan123@DESKTOP-L1KOBQK>
2024-05-07 15:52:10 +0500

    feat: Migrations
    change field "body" type from string to text

--- database/migrations/2024_05_06_121458_create_posts_table.php
+++ database/migrations/2024_05_06_121458_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->uuid("id")->primary();
             $table->string("title");
-            $table->string("body");
+            $table->text("body");
             $table->foreignUuid("user_id")->references("id")->on("users");
             $table->timestamps();
             $table->softDeletes();
