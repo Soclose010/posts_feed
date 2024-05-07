1c5248eebc25c42948aafa705b4c8a0d13f6b371
Stepan <pirogov1923@mail.ru>
2024-05-07 17:02:31 +0500

    feat: Gates
    Add edit user and edit post gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,25 +2,25 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         //
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
-        //
+        Gate::define('edit-user', function (User $authUser, User $editedUser) {
+            return $authUser->isAdmin() || $authUser->id === $editedUser->id;
+        });
+
+        Gate::define('edit-post', function (User $authUser, Post $editedPost) {
+            return $authUser->isAdmin() || $authUser->id === $editedPost->user_id;
+        });
     }
 }
