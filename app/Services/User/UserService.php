e5ccebc4e1620dad690b3fa233d4ececa65e2b39
Stepan <pirogov1923@mail.ru>
2024-05-10 06:19:22 +0500

    feat: UserServices
    Update method in service. Add Action log

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -3,51 +3,102 @@
 namespace App\Services\User;
 
 use App\DataTransferObjects\UserDto;
+use App\Enums\Action;
+use App\Enums\FieldName;
+use App\Enums\UserRole;
 use App\Exceptions\ExistedEmailException;
 use App\Models\User;
+use App\Services\Action\ActionServiceInterface;
 use App\Traits\FilterFieldsTrait;
 use Illuminate\Database\UniqueConstraintViolationException;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Spatie\FlareClient\Http\Exceptions\NotFound;
 
 class UserService
 {
     use FilterFieldsTrait;
+
+    public function __construct(private readonly ActionServiceInterface $actionService)
+    {
+    }
+
     public function create(UserDto $dto): UserDto
     {
         $user = User::create($this->fieldsToUpdate($dto));
+        $this->actionService::write(
+            Auth::id() ?? null,
+            $user->id,
+            Action::Create,
+            null,
+            $user
+        );
         return UserDto::fromModel($user);
     }
 
+    /**
+     * @throws NotFound
+     * @throws ExistedEmailException
+     */
     public function update(UserDto $dto): UserDto
     {
-        $user = $this->getUser($dto->id);
+        $user = $oldUser = $this->getUser($dto->id);
         Gate::authorize("edit", $user->id);
         try {
             $user = tap($user->fill($this->fieldsToUpdate($dto)))->save();
-        }
-        catch (UniqueConstraintViolationException)
-        {
+        } catch (UniqueConstraintViolationException) {
             throw new ExistedEmailException();
         }
+        $this->actionService::write(
+            Auth::id(),
+            $user->id,
+            Action::Update,
+            $oldUser,
+            $user
+        );
         return UserDto::fromModel($user);
     }
 
+    /**
+     * @throws NotFound
+     */
     public function delete(string $id): void
     {
         Gate::authorize("edit", $id);
-        User::where('id', $id)->delete();
+        $user = $this->getUser($id);
+        $user->delete();
+        $this->actionService::write(
+            Auth::id(),
+            $user->id,
+            Action::Delete,
+            $user,
+            null
+        );
     }
 
-    public function get(string $id): ?UserDto
+    /**
+     * @throws NotFound
+     */
+    public function get(string $value, FieldName $field = FieldName::Id): UserDto
     {
-        if ($user = User::where("id", $id)->first()) {
-            return UserDto::fromModel($user);
+        return UserDto::fromModel($this->getUser($value, $field));
+    }
+
+    public function upgrade(string $id): void
+    {
+        $user = $this->getUser($id);
+        $user->role = UserRole::Admin;
+        $user->save();
+    }
+
+    /**
+     * @throws NotFound
+     */
+    public function getUser(string $value, FieldName $field = FieldName::Id): User
+    {
+        if (!$user = User::where($field->value, $value)->first()) {
+            throw new NotFound();
         }
-        return null;
-    }
-
-    public function getUser(string $id): ?User
-    {
-        return User::find($id);
+        return $user;
     }
 }
