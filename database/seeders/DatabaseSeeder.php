7aae4d923db64cb74f2a429b250fdd338f2717d4
Stepan Filippov <pirogov1923@mail.ru>
2024-05-21 16:15:58 +0500

    feat: Seeds
    Add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,27 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::factory(3)
-            ->has(Post::factory(5))
+        User::factory(20)
+            ->has(Post::factory(10))
             ->create();
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
